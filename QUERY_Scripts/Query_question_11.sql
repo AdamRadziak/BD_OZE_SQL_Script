0c5cc3bf15e42af6a010ef29a7da0dc28c8e3601
AdamRadziak <adam.radziak@gmail.com>
2024-02-22 20:31:47 +0100

    fix some mistakes in queries and fix count weekdays

--- QUERY_Scripts/Query_question_11.sql
+++ QUERY_Scripts/Query_question_11.sql
@@ -1,6 +1,7 @@
 select
 R.Name
 , DS.StatusName
+, COUNT(D.Name) as Dev_count
 , SUM(D.MaxEnergyProduced) as Available_energy_prod
 , SUM(OD.EnergyDemand) as Energy_demand
 , dbo.Is_Energy_Prod_More_than_Energy_Demand(SUM(D.MaxEnergyProduced),SUM(OD.EnergyDemand),DS.StatusName) as Is_Order_possible_to_implement
@@ -10,6 +11,6 @@ join Regions R on R.IdDevice=D.IdDevice
 join RegionOrders RO on R.IdRegion=RO.IdRegion
 join OrderDetails OD on OD.IdRO=RO.IdRegionOrders
 WHERE RO.IdRegion=R.IdRegion
-GROUP BY R.Name,DS.StatusName
+GROUP BY DS.StatusName, R.Name
 
 
--- QUERY_Scripts/Query_question_9.sql
+++ QUERY_Scripts/Query_question_9.sql
@@ -4,7 +4,7 @@ D.Name as DevName
 , DS.StatusDescription as DevStatusDesc
 , F.StartDate as FailDate
 , F.EndDate as RepairDate
-, dbo.CountNonWorkingShifts(F.StartDate,F.EndDate) as FailShiftCOunt
+, dbo.CountNonWorkingShifts(F.StartDate,F.EndDate) as FailShiftCount
 FROM Devices D
 join Dev_Statuses DS on DS.IdDeviceStatus=D.IdDeviceStatus
 join Failures F on F.IdFailure=DS.IdFailure
