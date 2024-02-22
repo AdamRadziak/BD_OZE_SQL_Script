e1dd36185f4a1afe417037f3e421675b75675c49
AdamRadziak <adam.radziak@gmail.com>
2024-02-22 15:17:31 +0100

    update query for question 12 to example of using trigger

--- QUERY_Scripts/Query_question_12.sql
+++ QUERY_Scripts/Query_question_12.sql
@@ -1,13 +1,15 @@
 --example of using trigger
 UPDATE Dev_Statuses
-Set StatusName=0
+Set StatusName=1
+WHERE IdFailure BETWEEN 12 AND 13
 
 Select * from Dev_Statuses
 ORDER BY IdFailure
 Select *  from ErrorLogs
-
+--reset to default
 UPDATE Dev_Statuses
-Set StatusName=3
-WHERE IdFailure BETWEEN 7 AND 9
+Set StatusName=0
+WHERE IdFailure BETWEEN 12 AND 13
+Select * from Dev_Statuses
+ORDER BY IdFailure
 Select *  from ErrorLogs
---delete  from ErrorLogs
